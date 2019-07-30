fb755fc65e21cc59996969397dcce1b7214264f7
Quentin Perret <quentin.perret@arm.com>
2019-07-30 13:54:00 +0100

    ANDROID: sched/core: Add a latency-sensitive flag to uclamp
    Add a 'latency_sensitive' flag to uclamp in order to express the need
    for some tasks to find a CPU where they can wake-up quickly. This is not
    expected to be used without cgroup support, so add solely a cgroup
    interface for it.
    
    As this flag represents a boolean attribute and not an amount of
    resources to be shared, it is not clear what the delegation logic should
    be. As such, it is kept simple: every new cgroup starts with
    latency_sensitive set to false, regardless of the parent.
    
    In essence, this is similar to SchedTune's prefer-idle flag which was
    used in android-4.19 and prior.
    
    Change-Id: I722d8ecabb428bb7b95a5b54bc70a87f182dde2a
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -9263,6 +9263,27 @@ static int cpu_uclamp_max_show(struct seq_file *sf, void *v)
 	cpu_uclamp_print(sf, UCLAMP_MAX);
 	return 0;
 }
+
+static int cpu_uclamp_ls_write_u64(struct cgroup_subsys_state *css,
+				   struct cftype *cftype, u64 ls)
+{
+	struct task_group *tg;
+
+	if (ls > 1)
+		return -EINVAL;
+	tg = css_tg(css);
+	tg->latency_sensitive = (unsigned int) ls;
+
+	return 0;
+}
+
+static u64 cpu_uclamp_ls_read_u64(struct cgroup_subsys_state *css,
+				  struct cftype *cft)
+{
+	struct task_group *tg = css_tg(css);
+
+	return (u64) tg->latency_sensitive;
+}
 #endif /* CONFIG_UCLAMP_TASK_GROUP */
 
 #ifdef CONFIG_FAIR_GROUP_SCHED
@@ -9590,6 +9611,26 @@ static struct cftype cpu_files[] = {
 		.name = "rt_period_us",
 		.read_u64 = cpu_rt_period_read_uint,
 		.write_u64 = cpu_rt_period_write_uint,
+	},
+#endif
+#ifdef CONFIG_UCLAMP_TASK_GROUP
+	{
+		.name = "uclamp.min",
+		.flags = CFTYPE_NOT_ON_ROOT,
+		.seq_show = cpu_uclamp_min_show,
+		.write = cpu_uclamp_min_write,
+	},
+	{
+		.name = "uclamp.max",
+		.flags = CFTYPE_NOT_ON_ROOT,
+		.seq_show = cpu_uclamp_max_show,
+		.write = cpu_uclamp_max_write,
+	},
+	{
+		.name = "uclamp.latency_sensitive",
+		.flags = CFTYPE_NOT_ON_ROOT,
+		.read_u64 = cpu_uclamp_ls_read_u64,
+		.write_u64 = cpu_uclamp_ls_write_u64,
 	},
 #endif
 	{ }	/* terminate */
